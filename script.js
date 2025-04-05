3d967b26d6ed47a97d986c24596ec867ba58db91
Eyaas8 <eynar56789@gmail.com>
2025-04-04 23:44:00 -0500

    script.js

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 console.log("Sitio web ACEIG UNSA cargado correctamente.");
 
-// Mensaje al hacer clic en las imágenes
+// Alerta al hacer clic en imágenes de actividades
 document.querySelectorAll(".galeria img").forEach((img) => {
   img.addEventListener("click", () => {
     alert("¡Esta es una actividad reciente de ACEIG UNSA!");
   });
 });
+
 
 
 
